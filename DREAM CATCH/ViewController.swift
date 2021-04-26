7f874898743883b8603424671832f29a240e1944
takanotakuya <tmw9898@icloud.com>
2021-04-26 11:13:19 +0900

    Initial Commit

---
+++ DREAM CATCH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DREAM CATCH
+//
+//  Created by 髙野拓弥 on 2021/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DREAM CATCHTests/DREAM_CATCHTests.swift
@@ -0,0 +1,33 @@
+//
+//  DREAM_CATCHTests.swift
+//  DREAM CATCHTests
+//
+//  Created by 髙野拓弥 on 2021/04/26.
+//
+
+import XCTest
+@testable import DREAM_CATCH
+
+class DREAM_CATCHTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
