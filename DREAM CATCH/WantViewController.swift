a4e1f4ce8c43284e98eba07638acf3aef9723675
takanotakuya <tmw9898@icloud.com>
2021-04-27 11:53:32 +0900

    格ページ紐付け

---
+++ DREAM CATCH/WantViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WantViewController.swift
+//  DREAM CATCH
+//
+//  Created by 髙野拓弥 on 2021/04/27.
+//
+
+import UIKit
+
+class WantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
