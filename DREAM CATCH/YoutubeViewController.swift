f2f3e81334349b8c49c826a535fd966ae215c758
takanotakuya <tmw9898@icloud.com>
2021-04-28 19:33:58 +0900

    共通処理コードまとめる

--- DREAM CATCH/YoutubeViewController.swift
+++ DREAM CATCH/YoutubeViewController.swift
@@ -27,42 +27,50 @@ class YoutubeViewController: UIViewController {
     }
     
     private func fetchYoutubeSerachInfo() {
-        let urlString = "https://www.googleapis.com/youtube/v3/search?q=lebronjames&key=AIzaSyBMchvJKgYkEVGgw2BdAcT9q_-y6pu9QHA&part=snippet"
+        let path = "search"
+        let params = ["q": "lebronjames"]
         
-        let request = AF.request(urlString)
-        
-        request.responseJSON { (response) in
-            do {
-                guard let data = response.data else { return }
-                let decode = JSONDecoder()
-                let video = try decode.decode(Video.self, from: data)
-                self.videoItems = video.items
-                
-                let id = self.videoItems[0].snippet.channelId
-                self.fetchYoutubeChannelInfo(id: id)
-                
-            } catch {
-                print("変換に失敗しました。: ", error)
-            }
+        apiRequest(path: path, params: params, type: Video.self) { (video) in
+            
+            self.videoItems = video.items
+            let id = self.videoItems[0].snippet.channelId
+            self.fetchYoutubeChannelInfo(id: id)
         }
     }
     
     private func fetchYoutubeChannelInfo(id: String) {
-        let urlString = "https://www.googleapis.com/youtube/v3/channels?key=AIzaSyBMchvJKgYkEVGgw2BdAcT9q_-y6pu9QHA&part=snippet&id=\(id)"
+        let path = "channels"
+        let params = [
+            "id": id
+        ]
         
-        let request = AF.request(urlString)
+        apiRequest(path: path, params: params, type: Channel.self) { (channel) in
+            self.videoItems.forEach { (item) in
+                item.channel = channel
+            }
+            
+            self.videoListCollectionView.reloadData()
+        }
+    }
+    
+    private func apiRequest<T: Decodable>(path: String, params: [String: Any], type: T.Type, completion: @escaping (T) -> Void) {
+        let baseUrl = "https://www.googleapis.com/youtube/v3/"
+        let path = path
+        let url = baseUrl + path + "?"
+        
+        var params = params
+        params["key"] = "AIzaSyBMchvJKgYkEVGgw2BdAcT9q_-y6pu9QHA"
+        params["part"] = "snippet"
+        
+        let request = AF.request(url, method: .get, parameters: params)
         
         request.responseJSON { (response) in
             do {
                 guard let data = response.data else { return }
                 let decode = JSONDecoder()
-                let channel = try decode.decode(Channel.self, from: data)
-                self.videoItems.forEach { (item) in
-                    item.channel = channel
-                }
-                    
-                self.videoListCollectionView.reloadData()
+                let value = try decode.decode(T.self, from: data)
                 
+                completion(value)
             } catch {
                 print("変換に失敗しました。: ", error)
             }
