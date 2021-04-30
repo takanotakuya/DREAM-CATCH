26eb46d1f5809430317dbc7772a6edc259372f3b
takanotakuya <tmw9898@icloud.com>
2021-04-30 16:20:34 +0900

    アニメーションの制約・設定

--- DREAM CATCH/YoutubeViewController.swift
+++ DREAM CATCH/YoutubeViewController.swift
@@ -11,16 +11,38 @@ import Nuke
 class YoutubeViewController: UIViewController {
     
     var selectedItem: Item?
+    private var imageViewCenterY: CGFloat?
     
+    var videoImageMaxY: CGFloat {
+        let ecludeValue = view.safeAreaInsets.bottom + (imageViewCenterY ?? 0)
+        return view.frame.maxY - ecludeValue
+    }
+    
+    var minimumImageViewTrailingConstant: CGFloat {
+        view.frame.width - (150 + 12)
+    }
+    
+    // videoImageView
     @IBOutlet weak var videoImageView: UIImageView!
-    @IBOutlet weak var channelImageView: UIImageView!
-    @IBOutlet weak var videoTitleLabel: UILabel!
-    @IBOutlet weak var channelTitleLabel: UILabel!
-    @IBOutlet weak var backView: UIView!
-    @IBOutlet weak var baseBackGroundView: UIView!
     @IBOutlet weak var videoImageViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var videoImageViewLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var videoImageViewTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var videoImageBackView: UIView!
+    
+    // backView
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var backViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backViewTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backViewBottomConstraint: NSLayoutConstraint!
+    
+    // describeView
+    @IBOutlet weak var describeView: UIView!
+    @IBOutlet weak var describeViewTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var channelImageView: UIImageView!
+    @IBOutlet weak var videoTitleLabel: UILabel!
+    @IBOutlet weak var channelTitleLabel: UILabel!
+    @IBOutlet weak var baseBackGroundView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +61,10 @@ class YoutubeViewController: UIViewController {
     }
     
     private func setupViews() {
+        self.view.bringSubviewToFront(videoImageView)
+        
+        imageViewCenterY = videoImageView.center.y
+        
         channelImageView.layer.cornerRadius = 45 / 2
         
         if let url = URL(string: selectedItem?.snippet.thumbnails.medium.url ?? "") {
@@ -64,7 +90,13 @@ class YoutubeViewController: UIViewController {
         
         if gesture.state == .changed {
             
+            if videoImageMaxY <= move.y {
+                moveToBottom(imageView: imageView as! UIImageView)
+                return
+            }
+            
             imageView.transform = CGAffineTransform(translationX: 0, y: move.y)
+            videoImageBackView.transform = CGAffineTransform(translationX: 0, y: move.y)
             
             // 左右のpadding設定
             let movingConstant = move.y / 30
@@ -72,6 +104,8 @@ class YoutubeViewController: UIViewController {
             if movingConstant <= 12 {
                 videoImageViewTrailingConstraint.constant = -movingConstant
                 videoImageViewLeadingConstraint.constant = movingConstant
+                
+                backViewTrailingConstraint.constant = -movingConstant
             }
             
             // imageViewの高さの動き
@@ -79,14 +113,25 @@ class YoutubeViewController: UIViewController {
             let parantViewHeight = self.view.frame.height
             let heightRatio = 210 / (parantViewHeight - (parantViewHeight / 6))
             let moveHeight = move.y * heightRatio
-
+            
+            backViewTopConstraint.constant = move.y
             videoImageViewHeightConstraint.constant = 280 - moveHeight
-
+            describeViewTopConstraint.constant = move.y * 0.8
+            
+            let bottomMoveY = parantViewHeight - videoImageMaxY
+            let bottomMoveRatio = bottomMoveY / videoImageMaxY
+            let bottomMoveConstant = move.y * bottomMoveRatio
+            backViewBottomConstraint.constant = bottomMoveConstant
+            
+            // alpha値の設定
+            let alphaRatio = move.y / (parantViewHeight / 2)
+            describeView.alpha = 1 - alphaRatio
+            baseBackGroundView.alpha = 1 - alphaRatio
+            
             // imageViewの横幅の動き 150(最小値)
             let originalWidth = self.view.frame.width
-            let minimumImageViewTrailingConstant = originalWidth - (150 + 12)
             let constant = originalWidth - move.y
-
+            
             if minimumImageViewTrailingConstant > constant {
                 videoImageViewTrailingConstraint.constant = minimumImageViewTrailingConstant
                 return
@@ -97,20 +142,54 @@ class YoutubeViewController: UIViewController {
             }
             
         } else if gesture.state == .ended {
-            UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: [], animations: {
-                
-                self.backToIdentityAllViews(imageView: imageView as! UIImageView)
-            })
+            
+            if move.y < self.view.frame.height / 3 {
+                UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: [], animations: {
+                    
+                    self.backToIdentityAllViews(imageView: imageView as! UIImageView)
+                })
+            } else {
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: []) {
+                    
+                    self.moveToBottom(imageView: imageView as! UIImageView)
+                    
+                }
+            }
         }
     }
     
+    private func moveToBottom(imageView: UIImageView) {
+        // imageViewの設定
+        imageView.transform = CGAffineTransform(translationX: 0, y: videoImageMaxY)
+        videoImageViewTrailingConstraint.constant = minimumImageViewTrailingConstant
+        videoImageViewHeightConstraint.constant = 70
+        
+        videoImageBackView.transform = CGAffineTransform(translationX: 0, y: videoImageMaxY)
+        describeView.alpha = 0
+        backView.alpha = 0
+        baseBackGroundView.alpha = 0
+    }
+    
     private func backToIdentityAllViews(imageView: UIImageView) {
+        // imageViewの設定
         imageView.transform = .identity
-        self.videoImageViewHeightConstraint.constant = 280
-        self.videoImageViewLeadingConstraint.constant = 0
-        self.videoImageViewTrailingConstraint.constant = 0
+        videoImageViewHeightConstraint.constant = 280
+        videoImageViewLeadingConstraint.constant = 0
+        videoImageViewTrailingConstraint.constant = 0
+        
+        // backViewの設定
+        backViewTrailingConstraint.constant = 0
+        backViewBottomConstraint.constant = 0
+        backViewTopConstraint.constant = 0
+        backView.alpha = 1
+        
+        // describeViewの設定
+        describeViewTopConstraint.constant = 0
+        describeView.alpha = 1
+        
+        baseBackGroundView.alpha = 1
         
         self.view.layoutIfNeeded()
     }
-
+    
 }
