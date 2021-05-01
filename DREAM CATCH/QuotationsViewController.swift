312c7e4676dca0c7c22ef6e3ae934f35113977a4
takanotakuya <tmw9898@icloud.com>
2021-05-01 15:09:30 +0900

    ビューの作成

---
+++ DREAM CATCH/QuotationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuotationsViewController.swift
+//  DREAM CATCH
+//
+//  Created by 髙野拓弥 on 2021/05/01.
+//
+
+import UIKit
+
+class QuotationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+}
