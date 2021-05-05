21f840171909f6a52a7539225be45feb55709d64
takanotakuya <tmw9898@icloud.com>
2021-05-05 20:01:22 +0900

    コンソール上での情報取得

--- DREAM CATCH/JokesViewController.swift
+++ DREAM CATCH/JokesViewController.swift
@@ -1,59 +1,73 @@
-////
-////  QuotationsViewController.swift
-////  DREAM CATCH
-////
-////  Created by 髙野拓弥 on 2021/05/01.
-////
+//
+//  JokeViewController.swift
+//  DREAM CATCH
+//
+//  Created by 髙野拓弥 on 2021/05/03.
+//
 
 import UIKit
 import Foundation
 
-struct Quotations: Codable {
-    let quote: String
-    let author: String
+struct Jokes: Codable {
+    let contents: Contents
+    
+//    enum CodingKeys: String, CodingKey {
+//        case contents = "contents"
+//    }
+    
+    struct Contents: Codable {
+        let categories: [Category]
+        
+//        enum CodingKeys: String, CodingKey {
+//            case categories = "categories"
+//        }
+        
+        struct Category: Codable {
+            let description: String
 
-    enum CodingKeys: String, CodingKey {
-        case quote = "quote"
-        case author = "author"
+//            enum CodingKeys: String, CodingKey {
+//                case description = "description"
+//            }
+        }
     }
 }
 
-class QuotationsViewController: UIViewController {
-
+class JokesViewController: UIViewController {
+    
     private let cellId = "cellId"
-    private var quotations = [Quotations]()
-
+    private var jokes = [Category]()
+    
     let tableView: UITableView = {
         let tv = UITableView()
         return tv
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.addSubview(tableView)
         tableView.frame.size = view.frame.size
         tableView.backgroundColor = .black
         tableView.separatorColor = .lightGray
         tableView.separatorInset.left = 0
-
-        navigationItem.title = "名言集"
-
+        
+        navigationItem.title = "元気がでる言葉"
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register( UITableViewCell.self, forCellReuseIdentifier: cellId)
-
+        
         let headers = [
             "x-rapidapi-key": "d9943d2979mshdb102131da6e048p187e0fjsn83064e35795c",
-            "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com"
+            "x-rapidapi-host": "jokes.p.rapidapi.com"
         ]
-
-        let request = NSMutableURLRequest(url: NSURL(string: "https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=10&cat=famous")! as URL,
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://jokes.p.rapidapi.com/jod/categories")! as URL,
                                           cachePolicy: .useProtocolCachePolicy,
                                           timeoutInterval: 10.0)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
-
+        
         let session = URLSession.shared
         let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in
             if (error != nil) {
@@ -62,46 +76,44 @@ class QuotationsViewController: UIViewController {
                 let httpResponse = response as? HTTPURLResponse
                 print("httpResponse: ",httpResponse)
             }
-
+            
             if let data = data {
                 do {
-                    let quotation = try JSONDecoder().decode([Quotations].self, from: data)
-                    self.quotations = quotation
+                    let joke = try JSONDecoder().decode(Jokes.self, from: data)
+//                    self.jokes = joke.contents.categories
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
+                        print("json: ", joke)
                     }
-                    print("json: ", quotation)
-                } catch(let err) {
-                    print("情報の取得に失敗しました。:", err)
+                    
+                } catch(let error) {
+                    print("情報の取得に失敗しました。:", error)
                 }
             }
         })
-
+        
         dataTask.resume()
+        
     }
-
+    
 }
 
-extension QuotationsViewController: UITableViewDelegate, UITableViewDataSource {
+extension JokesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        return 200
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  quotations.count
+        return jokes.description.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cellId")
-        print("quotations: " ,quotations)
+        print("jokes: " ,jokes)
 
-        cell.textLabel?.text = quotations[indexPath.row] .quote
+//        cell.textLabel?.text = jokes[indexPath.row] .description
         cell.textLabel?.textColor = .white
         cell.textLabel?.numberOfLines = 0
-
-        cell.detailTextLabel?.text = quotations[indexPath.row] .author
-        cell.detailTextLabel?.textColor = .lightGray
-        cell.detailTextLabel?.numberOfLines = 0
 
         cell.backgroundColor = .black
         cell.selectionStyle = UITableViewCell.SelectionStyle.none
